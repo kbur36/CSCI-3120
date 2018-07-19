d71ac0b044e718ca297c51e7dc20db05e48a33f2
kbur36 <34165438+kbur36@users.noreply.github.com>
2018-07-19 18:31:55 -0300

    COMPILES

--- A3/a3.c
+++ A3/a3.c
@@ -1,42 +1,73 @@
 #include <stdio.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+
 
 sem_t sema;
 pthread_mutex_t mutA;
 pthread_mutex_t mutB;
 
-void cafeteria();
-void wating_room();
-void exam_room();
+int rand_num();
+void *cafeteria();
+void *waiting_room();
+void *exam_room();
 
 int main(int argc, char const *argv[]) {
 	// mutexA
-	pthread_mutex_init(mutA, NULL);
+	pthread_mutex_init(&mutA, NULL);
 	// mutexB
-	pthread_mutex_init(mutB, NULL);
+	pthread_mutex_init(&mutB, NULL);
 	// semaphore
-	sem_init(sema, 0, 3);
+	sem_init(&sema, 0, 3);
 	// 5 patient pthreads
 	pthread_t p1;
 	pthread_t p2;
 	pthread_t p3;
 	pthread_t p4;
 	pthread_t p5;
+
 	// 1 doctor pthread
 	pthread_t doc;
 
-	pthread_create(&p1, NULL, cafeteria(), NULL);
-	pthread_create(&p2, NULL, cafeteria(), NULL);
-	pthread_create(&p3, NULL, cafeteria(), NULL);
-	pthread_create(&p4, NULL, cafeteria(), NULL);
-	pthread_create(&p5, NULL, cafeteria(), NULL);
+	pthread_create(&p1, NULL, (void *)cafeteria(), NULL);
+	pthread_create(&p2, NULL, (void *)cafeteria(), NULL);
+	pthread_create(&p3, NULL, (void *)cafeteria(), NULL);
+	pthread_create(&p4, NULL, (void *)cafeteria(), NULL);
+	pthread_create(&p5, NULL, (void *)cafeteria(), NULL);
 	
-
-
 	return 0;
 }
 
+void *cafeteria() {
+	// update semaphore
+	printf("%s\n", );
+	sem_post(&sema);
+	waiting_room();
+
+}
+
+void *waiting_room() {
+	// Lock examination room
+	sem_wait(&sema);
+	// update semaphore
+	sem_post(&sema);
+	exam_room();
+}
+
+void *exam_room() {
+	// Sleep for a random amount of time
+	sleep(rand_num()); 
+	// unlock B
+	sem_post(&sema);
+}
+
+int rand_num() {
+	int r = rand() % 20;
+	return r;
+}
 
 // Instantiate global semaphore and mutex A and B
 // semaphore s
@@ -50,25 +81,3 @@ int main(int argc, char const *argv[]) {
 // 	create semaphore allowing a maximum of 3 threads at any time, using mutex A
 // 	create threads passing the cafeteria() function as a parameter
 // }
-
-void cafeteria() {
-	update semaphore
-	waiting_room()
-}
-
-void wating_room() {
-	lock mutex B // Lock examination room
-	update semaphore
-	exam_room()
-}
-
-void exam_room() {
-	sleep(thread p) // for a random amount of time
-	unlock B
-}
-
-void be_a_doctor() {
-	
-}
-
-
